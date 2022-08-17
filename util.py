6024031d8db98bb0894a38dee9b2a9d816c468b2
Hellcreams <47096303+Hellcreams@users.noreply.github.com>
2022-08-18 01:37:59 +0900

    Create util.py

---
+++ util.py
@@ -0,0 +1,23 @@
+import datetime
+import discord
+
+def seconds_until(hours, minutes):
+    given_time = datetime.time(hours, minutes)
+    now = datetime.datetime.now()
+    future_exec = datetime.datetime.combine(now, given_time)
+    if (future_exec - now).days < 0:  # If we are past the execution, it will take place tomorrow
+        future_exec = datetime.datetime.combine(now + datetime.timedelta(days=1), given_time) # days always >= 0
+
+    return (future_exec - now).total_seconds()
+
+
+def lprint(*args, sep=" ", end="\n"):
+    print(datetime.datetime.now().strftime('[%H:%M:%S]'), end=" ")
+    print(*args, sep=sep, end=end)
+
+
+def get_args(msg):
+    if isinstance(msg, discord.Message):
+        return msg.content.split(' ')
+    else:
+        raise ValueError("The argument is not discord.Message")
